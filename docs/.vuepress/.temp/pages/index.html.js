0128b17703a8968875c76ad7aaf3b006ef0c0045
qiuliw <1807191473@qq.com>
2024-03-18 06:09:36 +0800

    不同步依赖

--- docs/.vuepress/.temp/pages/index.html.js
+++ docs/.vuepress/.temp/pages/index.html.js
@@ -1,5 +1,5 @@
 import comp from "/Users/qiuliwei/devTools/Github/vuepress2.0/docs/.vuepress/.temp/pages/index.html.vue"
-const data = JSON.parse("{\"path\":\"/\",\"title\":\"Vuepress2.0 Demo\",\"lang\":\"zh-CN\",\"frontmatter\":{},\"headers\":[],\"git\":{},\"filePathRelative\":\"README.md\"}")
+const data = JSON.parse("{\"path\":\"/\",\"title\":\"Vuepress2.0 Demo\",\"lang\":\"zh-CN\",\"frontmatter\":{},\"headers\":[],\"git\":{\"updatedTime\":1710712121000,\"contributors\":[{\"name\":\"qiuliw\",\"email\":\"1807191473@qq.com\",\"commits\":1}]},\"filePathRelative\":\"README.md\"}")
 export { comp, data }
 
 if (import.meta.webpackHot) {
--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,12 +1,21 @@
 import { viteBundler } from '@vuepress/bundler-vite'
 import { defaultTheme } from '@vuepress/theme-default'
+import { text } from 'stream/consumers'
 import { defineUserConfig } from 'vuepress'
 
 export default defineUserConfig({
   //打包工具
   bundler: viteBundler(),
   //主题
-  theme: defaultTheme(),
+  theme: defaultTheme({
+    docsBranch: 'master',
+    navbar: [{
+        text: 'Gitee',
+        link: 'https://qiuliw.gitee.io/vuepress2.0'
+    }],
+    repo: 'qiuliw/vuepress2.0' //仓库链接
+  }),
+
 
   lang: 'zh-CN',
   title: '你好， VuePress ！',
